d8383fa6ecc753546b5e69c30694ddc794507b12
pipisa <your@email.ex>
2025-02-26 16:23:01 +0200

    pievienots use..AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,4 +7,17 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     //
+    public function register(Request $request){
+        
+        return 'register';
+    }
+
+    public function login(Request $request){
+        return 'login';
+    }
+
+    public function logout(Request $request){
+        return 'logout';
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -14,3 +15,6 @@ Route::apiResource('posts', PostController::class);
 //     return 'api';
 // });
   
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
